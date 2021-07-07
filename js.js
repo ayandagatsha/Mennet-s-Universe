049c412c29ec38bbeba81a195881dd6d89636c01
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-07-07 03:16:11 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+let men = document.getElementById("gif");
+
+window.onload = function(){
+    setInterval(function(){
+        men.className="aura mennet";
+    }, 5000);
+ };
+
+
+    
+ window.onload = function(){
+    setInterval(function(){
+        men.className=" mennet";
+    }, 10000);
+ };
+
+
+
+
+
+
